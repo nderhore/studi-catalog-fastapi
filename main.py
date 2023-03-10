21bd6ab1aba9c21cf6ed11ae5fac8181e27b5e96
Nathan Derhore <nathan@derhore.fr>
2023-03-10 19:57:13 +0100

    implements CORS

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src.api import api
 from src.core.database.database import Base, engine
 
+
+origins = [
+    "http://localhost:4200"
+]
+
+
 app = FastAPI(title='Studi')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api.routeur, prefix='/api')
 Base.metadata.create_all(engine)
